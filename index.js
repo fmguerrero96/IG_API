db8c932a17df9161fd32ec7463bd8bd0fc0c5f55
Misael Guerrero <mguerrerowest@gmail.com>
2024-05-29 21:02:13 -0600

    Use json parser and cors middlewares

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
 
 const app = express();
 
 const PORT = (process.env.PORT || 2000);
+
+app.use(express.json());
+app.use(cors());
 
 app.listen(PORT, () => {
     console.log(PORT)
