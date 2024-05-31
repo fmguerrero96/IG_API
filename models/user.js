c9a0d174432cca5da6b46f5899c12e3f657071c7
Misael Guerrero <mguerrerowest@gmail.com>
2024-05-31 14:12:05 -0600

    Modify user model to make passwords at least 5 characters long

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const {Schema, model} = mongoose;
 
 const UserSchema = new Schema({
     username: {type: String, required: true, min: 4, unique: true},
-    password: {type: String, required: true},
+    password: {type: String, required: true, min: 5},
 })
 
 const UserModel = model('User', UserSchema);
