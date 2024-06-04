64276fe041ff2d676f1bf0603162d0493652089b
Misael Guerrero <mguerrerowest@gmail.com>
2024-06-03 18:01:51 -0600

    Create test route and authentication middleware function to protect routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const passport = require('passport');
+
+// authentication middleware function to protect routes
+const authenticateJWT = passport.authenticate('jwt', { session: false });
 
 const user_controller = require('../controllers/userController');
 
@@ -8,5 +12,8 @@ router.post('/users', user_controller.createUser);
 
 // Login
 router.post('/users/login', user_controller.login);
+
+//test
+router.get('/protected', authenticateJWT, user_controller.test)
 
 module.exports = router;
