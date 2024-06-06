a97b107ed5192a3317dfd57345456aef7b340c25
Misael Guerrero <mguerrerowest@gmail.com>
2024-06-05 21:12:41 -0600

    Create route to get info from a single user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,5 +13,7 @@ router.post('/users', user_controller.createUser);
 // Login
 router.post('/users/login', user_controller.login);
 
+// Send single user info (protected route)
+router.get('/user', authenticateJWT, user_controller.getUser);
 
 module.exports = router;
