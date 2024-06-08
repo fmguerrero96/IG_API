a0f0afaad37ddd5a056fadefff482a9db8a7aad7
Misael Guerrero <mguerrerowest@gmail.com>
2024-06-07 20:05:07 -0600

    Create a postController, postRoutes and post model files

---
+++ models/post.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const {Schema, model} = mongoose;
+
+const PostSchema = new Schema({
+    author: { type: Schema.Types.ObjectId, ref: "User"},
+    caption: {type: String, required: true, max: 150},
+    time_stamp: { type: Date, default: Date.now },
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+    likes_count: {type: Number, default: 0 },
+    picture: {type: },
+});
+
+const PostModel = model('Post', PostSchema);
+
+module.exports = PostModel;
