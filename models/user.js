7bf5c323c1ea4aae798e864067e18589d619803a
Misael Guerrero <mguerrerowest@gmail.com>
2024-06-07 21:38:57 -0600

    Add new 'posts' property to schema

--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ const {Schema, model} = mongoose;
 const UserSchema = new Schema({
     username: {type: String, required: true, min: 4, unique: true},
     password: {type: String, required: true, min: 5},
+    posts: {type: Array, default: []}
 })
 
 const UserModel = model('User', UserSchema);
