8e721d60a7ea6d5219edc6260ff0e44ea7aa618e
Misael Guerrero <mguerrerowest@gmail.com>
2024-06-11 10:47:19 -0600

    Expand User model by adding followers and following

--- models/user.js
+++ models/user.js
@@ -4,7 +4,9 @@ const {Schema, model} = mongoose;
 const UserSchema = new Schema({
     username: {type: String, required: true, min: 4, unique: true},
     password: {type: String, required: true, min: 5},
-    posts: [{ type: Schema.Types.ObjectId, ref: "Post" }]
+    posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+    followers: [{ type: Schema.Types.ObjectId, ref: "User"}],
+    following: [{ type: Schema.Types.ObjectId, ref: "User"}],
 })
 
 const UserModel = model('User', UserSchema);
