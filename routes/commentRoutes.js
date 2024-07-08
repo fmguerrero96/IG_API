09148b92417e8301bfde54369751c5344730065d
Misael Guerrero <mguerrerowest@gmail.com>
2024-07-08 14:11:08 -0600

    Create a comment routes file

---
+++ routes/commentRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const passport = require('passport');
+
+// authentication middleware function to protect routes
+const authenticateJWT = passport.authenticate('jwt', { session: false });
+
+const comment_controller = require('../controllers/commentController');
+
+
+
+module.exports = router;
