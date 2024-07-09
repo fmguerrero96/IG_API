a570bc35c81f20f1e6711fdd9ae86ac4e49ca688
Misael Guerrero <mguerrerowest@gmail.com>
2024-07-09 14:22:03 -0600

    Create route to fetch comments from a specific post

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -10,4 +10,7 @@ const comment_controller = require('../controllers/commentController');
 // Create new comment
 router.post('/comment/:postID', authenticateJWT, comment_controller.createComment);
 
+// Get comments from a post
+router.get('/comment/:postID', comment_controller.getComments);
+
 module.exports = router;
